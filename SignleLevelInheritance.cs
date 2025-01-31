7d774b5267e4620b4f833f3d80d59e96c4a34137
Manya Sharma <manya271001@gmail.com>
2025-01-31 09:53:02 +0530

    single and multilevel

---
+++ SignleLevelInheritance.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+
+
+
+
+
+
+//namespace ConsoleApp1WiproTraining
+//{
+//    class vehicle
+//    {
+//        public int number;
+//        public string name;
+
+//        public void displayData()
+//        {
+//            Console.WriteLine("vehicle detial function called : ");
+//        }
+//    }
+
+//    class car : vehicle
+//    {
+//        public void carInfo()
+//        {
+//            Console.WriteLine("car info function called");
+//        } 
+//    }
+//    class bike : vehicle
+//    {
+//        public void bikeInfo()
+//        {
+//            Console.WriteLine("bike info function called");
+//        }
+//    }
+//    internal class SignleLevelInheritance
+//    {
+//        static void Main(string[] args)
+//        {
+//            bike bike1 = new bike();
+//            bike1.number = 1;
+//            bike1.bikeInfo();
+//        }
+
+
+
+//    }
+//}
---
+++ multiLevelInheritance.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1WiproTraining
+{
+
+    class vehicle
+    {
+        public int number;
+        public string name;
+
+        public void displayData()
+        {
+            Console.WriteLine("vehicle detial function called : ");
+            }
+        }
+
+        class car : vehicle
+{
+    public void carInfo()
+    {
+        Console.WriteLine("car info function called");
+    }
+}
+class bike : car
+{
+    public void bikeInfo()
+    {
+        Console.WriteLine("bike info function called");
+    }
+}
+internal class multiLevelInheritance
+    {
+        static void Main(string[] args)
+        {
+            bike bike = new bike();
+            bike.carInfo();
+            bike.bikeInfo();
+            bike.displayData();
+            
+        }
+    }
+}
